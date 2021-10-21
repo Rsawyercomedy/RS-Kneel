a3c492ad336e583ffaf7b28443611e65868a1408
Rsawyercomedy <rsawyer7777@outlook.com>
2021-10-21 14:09:27 -0500

    added .map method on styles.js

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -12,7 +12,14 @@ export const JewelryStyles = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
-    const listItemsArray =
+    const listItemsArray = styles.map(
+        (style) => {
+            return  `<li>
+            <input type="radio" name="style" value="${style.id}" /> ${style.price}
+            </li>`
+    }
+    )
+    html += listItemsArray.join("")
 
 
     // Join all of the strings in the array into a single string
